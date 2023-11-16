3a74a96aa3bc9da11807202a418a6f76e6a91fe7
Lee Knight <leekengineering@gmail.com>
2023-11-16 12:05:26 +0000

    functional project styled and font added.

--- library.js
+++ library.js
@@ -6,11 +6,12 @@ function Book(title,author,pages,read) {
     this.pages = pages
     this.read = read
     this.info = function() {
-      return `${title} by ${author} ${pages} pages read ${read}`
+      return `${title} by ${author}
+       ${pages} pages`
     }
 }
-const thereAndBackAgain = new Book("The Hobbit", "JRR Tolkien", 310, true)
 
+const cross= document.querySelector(".close")
 const library= document.querySelector(".library");
 const addBook= document.querySelector(".submitBook");
 const title= document.querySelector(".title");
@@ -18,18 +19,78 @@ const author= document.querySelector(".author");
 const pages= document.querySelector(".pages");
 const read= document.querySelector(".read");
 
+cross.addEventListener("click", dontAddToLibrary);
 addBook.addEventListener("click", addBookToLibrary);
+
+function dontAddToLibrary() {
+ const dialog= document.querySelector("#dialog")
+ console.log("done")
+ dialog.close()
+}
 
 function addBookToLibrary() {
    const book = new Book(title.value, author.value, pages.value, read.value);
-   myLibrary.unshift(book);
-   const div= document.createElement("div");
+   myLibrary.push(book);
+   title.value= ""
+   author.value= ""
+   pages.value= ""
+   makeBook(book);
+}
+
+function makeBook(current) {
+  const div= document.createElement("div");
    library.appendChild(div);
-   div.textContent= book.info();
-   style(div)
+   div.classList.add(current.title);
+  
+   myLibrary.forEach(book => {
+   div.textContent= book.info()
+   });
+   buttons(div,current);
 }
 
 
-function style(div) {
-  div.classList.add("book")
+function buttons(div,book) {
+  const btnRemove= document.createElement("button");
+  btnRemove.textContent= "Remove";
+  btnRemove.id = book.title
+  btnRemove.addEventListener("click", removeBook)
+  div.appendChild(btnRemove);
+
+  const btnRead= document.createElement("button");
+  btnRead.textContent= read.value;
+  btnRead.id = book.author
+  btnRead.addEventListener("click", changeRead)
+  div.appendChild(btnRead);
+}
+
+
+function removeBook() {
+  const delTitle= this.id;
+  myLibrary.forEach(element);
+
+  function element(item, index) {
+    if(item.title===delTitle){
+     myLibrary.splice(index,1);
+    }
+  }
+  const ui= document.getElementById(delTitle).parentElement.remove()
+}
+
+function changeRead() {
+  const preChange= this.id;
+  const change= document.getElementById(preChange);
+  myLibrary.forEach(element);
+  
+  function element(item) {
+    if(item.author===preChange){
+
+      if(change.textContent==="Not read"){
+        item.read= "Read";
+        change.textContent= "Read";
+      }else if(change.textContent==="Read") {
+        item.read= "Not read"
+        change.textContent= "Not read"
+      }
+    }
+  } 
 }
